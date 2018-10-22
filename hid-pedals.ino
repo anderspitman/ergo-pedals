f8aacd0c7c93dcd59e11b21d5bae7fef21b6edd5
Anders Pitman <tapitman11@gmail.com>
2018-10-21 23:26:20 -0600

    Working with 2 pedals
    * Left pedal => left CTRL
    * Right pedal => left SHIFT
    * Both pedals => left META (Windows key)

--- hid-pedals.ino
+++ hid-pedals.ino
@@ -1,43 +1,118 @@
-uint8_t buf[8] = { 
-  0 };   /* Keyboard report buffer */
+ 
+#define NUM_SWITCHES 2
+
+#define CODE_LEFT_CTRL 0xe0
+#define CODE_LEFT_SHIFT 0xe1
+#define CODE_LEFT_META 0xe3
 
 #define PIN_LEFT_CTRL 5
+#define PIN_LEFT_SHIFT 4
 
-int state = 1;
-int prevState = 1;
+#define DEBOUNCE_MS 10
+
+#define HIGH 0
+#define LOW 1
+
+class Key {
+  private:
+    byte _state = 0;
+    bool _changed = false;
+    byte _pin = 4;
+    int _code = 0;
+    
+  public:
+
+    Key(byte pin, int code) {
+
+      this->_pin = pin;
+      this->_code = code;
+    }
+
+    void init() {
+      pinMode(this->_pin, INPUT);
+      // enable internal pull-ups
+      digitalWrite(this->_pin, 1);
+    }
+    
+    void update() {
+      byte state = digitalRead(this->_pin);
+      
+      if (state != this->_state) {
+        //Serial.print("Change: ");
+        //Serial.println(this->_pin);
+        //Serial.println(this->_state);
+        //Serial.println(state);
+        
+        this->_changed = true;
+        this->_state = state;
+      }  
+    }
+
+    byte state() {
+      return this->_state;
+    }
+
+    bool changed() {
+      const bool changed = this->_changed;
+      this->_changed = false;
+      return changed;
+    }
+
+    int code() {
+      return this->_code;
+    }
+};
+
+// Keyboard report buffer
+uint8_t buf[8] = { 0 };
+
+const Key KEYS[NUM_SWITCHES] = { Key(PIN_LEFT_CTRL, CODE_LEFT_CTRL), Key(PIN_LEFT_SHIFT, CODE_LEFT_SHIFT) };
 
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(PIN_LEFT_CTRL, INPUT);
-  // enable internal pull-ups
-  digitalWrite(PIN_LEFT_CTRL, 1); 
+
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    KEYS[i].init();
+  }
 
   delay(200);
 }
 
-void loop() 
-{
-  state = digitalRead(PIN_LEFT_CTRL);
+void loop() {
 
-  // rising edge
-  if (state == 0 && prevState == 1) {
-    pressKey();
-  }
-  // falling edge
-  else if (state == 1 && prevState == 0) {
-    releaseKey();
+  bool atLeastOneChanged = false;
+
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    
+    KEYS[i].update();
+    
+    if (KEYS[i].changed()) {
+      atLeastOneChanged = true;
+      break;
+      //Serial.println(buf[i + 2]);
+    }
   }
 
-  prevState = state;
+  if (atLeastOneChanged) {
+    if (KEYS[0].state() == HIGH && KEYS[1].state() == HIGH) {
+      buf[2] = CODE_LEFT_META;
+    }
+    else if (KEYS[0].state() == HIGH) {
+      buf[2] = CODE_LEFT_CTRL;
+    }
+    else if (KEYS[1].state() == HIGH) {
+      buf[2] = CODE_LEFT_SHIFT;
+    }
+    else {
+      buf[2] = 0;
+    }
+    sendUpdate();
+  }
 }
 
-void pressKey() {
-  buf[2] = 0xe0;   // Left CTRL key
-  Serial.write(buf, 8); // Send keypress
-}
-void releaseKey() {
-  buf[0] = 0;
-  buf[2] = 0;
-  Serial.write(buf, 8); // Release key  
+void sendUpdate() {
+  Serial.write(buf, 8);
+  //Serial.println(buf[2]);
+  //Serial.println(buf[3]);
 }
