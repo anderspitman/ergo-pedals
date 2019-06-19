d2b326286884e9e7e4555572cd5ce9f0092b0de8
Anders Pitman <tapitman11@gmail.com>
2019-06-19 07:39:49 -0600

    Add programming scripts

---
+++ set_for_hid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Uno R1
+chip=at90usb82
+# Uno R3
+#chip=atmega16u2
+
+sudo dfu-programmer ${chip} erase
+sudo dfu-programmer ${chip} flash hid.hex
+sudo dfu-programmer ${chip} reset
